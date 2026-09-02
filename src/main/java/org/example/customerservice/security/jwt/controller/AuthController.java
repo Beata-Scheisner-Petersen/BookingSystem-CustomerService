a8729d5112a3349d856b44e7fc729beb9315c14c
SleepingFores7s <Daniel.Evensson@hotmail.com>
2026-09-03 01:37:36 +0200

    Added JWT Creation/Authorizatio/dependencies/

---
+++ src/main/java/org/example/customerservice/security/jwt/controller/AuthController.java
@@ -0,0 +1,37 @@
+package org.example.customerservice.security.jwt.controller;
+
+import org.example.customerservice.customer.model.Customer;
+import org.example.customerservice.customer.service.CustomerService;
+import org.example.customerservice.security.jwt.service.JwtService;
+import org.example.customerservice.security.password.PasswordService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final JwtService jwt;
+    private final CustomerService service;
+    private final PasswordService bcrypt;
+
+    AuthController(JwtService jwt,  CustomerService service, PasswordService bcrypt) {
+        this.jwt = jwt;
+        this.service = service;
+        this.bcrypt = bcrypt;
+    }
+
+    record LoginDto(String email, String password){}
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDto dto){
+
+        Customer customer = service.getCustomerInformation(dto.email);
+
+        if(!bcrypt.matches(dto.password, customer.getPassword())) {
+            return jwt.generateToken(customer.getId());
+        }
+        throw new RuntimeException("Fel inloggning");
+    }
+}
